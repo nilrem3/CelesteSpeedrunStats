59e5a9f6195ef27b43141dc90af604f2da5af03c
nilrem3 <nilremnilrem@gmail.com>
2023-05-01 15:00:20 -0700

    checks if file changed

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import json
 import time
+import threading
+
 
 def check_settings():
     if os.path.isfile("./settings.json"):
-        #settings file exists
+        # settings file exists
         with open("./settings.json", "r") as f:
             return json.loads(f.read())
     else:
@@ -21,6 +23,7 @@ def check_settings():
             print("Process will exit now.")
             quit()
 
+
 def monitor_file_for_changes(path, interval, callback):
     last_time = 0
     last_data = ""
@@ -30,7 +33,7 @@ def monitor_file_for_changes(path, interval, callback):
         if not os.path.isfile(path):
             new_data = ""
         elif os.path.getmtime(path) > last_time:
-            #file was modified since we last checked
+            # file was modified since we last checked
             with open(path, "r") as f:
                 new_data = f.read()
         if new_data != last_data:
@@ -40,10 +43,28 @@ def monitor_file_for_changes(path, interval, callback):
         time.sleep(interval)
 
 
+def save_path_from_slot(saves_dir, slot):
+    return os.path.join(saves_dir, slot + ".celeste")
+
+
 def main():
-    #check if settings exists
+    # check if settings exists
     settings = check_settings()
-    
+
+    il_file_path = save_path_from_slot(settings["CelesteSaveFolder"], settings["ILSaveSlot"])
+    il_file_checker = threading.Thread(target=monitor_file_for_changes, args=(il_file_path, 0.1, lambda x: print("il file changed")))
+    il_file_checker.daemon = True
+    il_file_checker.start()
+
+    anypercent_file_path = save_path_from_slot(settings["CelesteSaveFolder"], settings["AnyPercentSaveSlot"])
+    anypercent_file_checker = threading.Thread(target=monitor_file_for_changes, args=(anypercent_file_path, 0.1, lambda x: print("any% file changed")))
+    anypercent_file_checker.daemon = True
+    anypercent_file_checker.start()
+
+    input("Press enter to stop")
+    il_file_checker.join()
+    anypercent_file_checker.join()
+
 
 if __name__ == "__main__":
     main()
