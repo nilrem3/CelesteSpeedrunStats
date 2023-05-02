a95ba63f9804452f02cda763fba215c5cb621f2f
nilrem3 <nilremnilrem@gmail.com>
2023-05-02 14:05:56 -0700

    cassette and crystal heart data

--- saveparser.py
+++ saveparser.py
@@ -36,12 +36,16 @@ class CelesteSaveData:
         self.chapter_completed = {}
         self.chapter_death_counts = {}
         self.checkpoints_completed = {}
+        self.hearts = {}
+        self.cassettes = {}
 
         for area_data in areas_data.findall("AreaStats"):
             chapter_id = int(area_data.get("ID"))
 
             sides = area_data.find("Modes").findall("AreaModeStats")
             has_sides = chapter_id in (1, 2, 3, 4, 5, 6, 7, 9)  # ids of levels that have b- and c-sides
+
+            self.cassettes[chapter_id] = area_data.get("Cassette") == "true"
 
             if has_sides:
                 for side_id in range(3):
@@ -53,6 +57,7 @@ class CelesteSaveData:
                     self.checkpoints_completed[level_id] = len(sides[side_id].find("Checkpoints").findall("string"))
                     if self.chapter_completed[level_id]:
                         self.checkpoints_completed += 1
+                    self.hearts[level_id] = sides[side_id].get("HeartGem") == "true"
             else:
                 level_id = LEVEL_CODE_BY_ID[chapter_id]
                 self.total_chapter_times_100_ns[level_id] = int(sides[0].get("TimePlayed"))
@@ -62,4 +67,5 @@ class CelesteSaveData:
                 self.checkpoints_completed[level_id] = len(sides[0].find("Checkpoints").findall("string"))
                 if self.chapter_completed[level_id]:
                     self.checkpoints_completed[level_id] += 1
+                self.hearts[level_id] = sides[0].get("HeartGem") == "true"
 
