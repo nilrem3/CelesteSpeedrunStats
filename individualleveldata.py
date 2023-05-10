a7a467af5f8502a72380bc63bea76ee760c91c61
nilrem3 <nilremnilrem@gmail.com>
2023-05-10 00:50:17 -0700

    Uploader class
    move uploading data out of rundata into its own class

--- individualleveldata.py
+++ individualleveldata.py
@@ -13,10 +13,7 @@ class CelesteIndividualLevelData:
     def __init__(self, settings):
         self.previous_save_data = None
         self.reset()
-        success = self.setup_sheet(settings)
-        if not success:
-            log_message(LogLevel.FATAL, "Failed to connect to google sheet.")
-            quit()
+        self.ready_to_upload = False
 
     # Reset all run data to empty
     def reset(self):
@@ -41,7 +38,9 @@ class CelesteIndividualLevelData:
         # If there is no previous data this is the first time updating the file, return early
         if self.previous_save_data is None:
             self.previous_save_data = save
+            self.ready_to_upload = False
             return
+        self.ready_to_upload = True
         if constants.ENDS_WITH_HEART[self.level_id] == False and self.end_room == constants.FINAL_ROOM_BY_LEVEL_ID[self.level_id]:
             # If we saved while in the last room of an a-side we completed the run
             print("Run completed with a time of {}".format(self.run_time))
@@ -58,50 +57,6 @@ class CelesteIndividualLevelData:
 
         self.previous_save_data = save
 
-        if not save.current_session_in_first_room:
-            self.upload_data_to_sheet()
-        self.reset()
-
-    def setup_sheet(self, settings) -> bool:
-        try:
-            gc = gspread.service_account(filename="credentials.json")
-        except OSError as e:
-            log_message(LogLevel.ERROR, "Failed to find credentials.json, make sure you have the file in the same directory as the exe, and named exactly 'credentials.json'")
-            return False
-        try:
-            sh = gc.open_by_url(
-                settings["SheetUrl"]
-            )
-        except gspread.exceptions.APIError:
-            log_message(LogLevel.ERROR, "Google Sheets API Error")
-            return False
-
-        try:
-            self.dataSheet = sh.worksheet("Raw Data")
-        except gspread.WorksheetNotFound:
-            log_message(LogLevel.ERROR, "No Worksheet 'Raw Data' Found.")
-            return False
-        return True
-
-
-    def upload_data_to_sheet(self):
-        self.dataSheet.insert_row(
-            [
-                self.run_date_and_time,
-                self.level_id,
-                self.run_time / 36000000000 / 24,
-                self.deaths,
-                self.dashes,
-                len(self.berries),
-                self.cassette,
-                self.heart,
-                self.golden,
-                self.end_room,
-                self.completed_run,
-            ],
-            index=2,
-            value_input_option="USER_ENTERED",
-        )
 
     def update_data_from_save(self, save):
         has_sides = save.current_session_id in (
